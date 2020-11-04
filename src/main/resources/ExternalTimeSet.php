70b760f2b545da25a9e6c79ca2b822cc257cf573
Uncovery <minecraft@uncovery.me>
2020-11-04 16:54:31 +0800

    cosmetics

--- src/main/resources/ExternalTimeSet.php
+++ src/main/resources/ExternalTimeSet.php
@@ -1,9 +1,9 @@
 <?php
     require_once 'Websend.php';
-    
+
     $ws = new Websend("123.456.789.123");
     $ws->password = "websendpassword";
-    
+
     if($ws->connect()){
         $ws->doCommandAsConsole("time set 6000");
         echo "Time set.";
@@ -11,4 +11,3 @@
         echo "Failed to connect.";
     }
     $ws->disconnect();
-?>
